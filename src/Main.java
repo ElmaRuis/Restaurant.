7d77bafade15480dc09991ed1269f4323ce17ea0
ElmaRuis <114675878+ElmaRuis@users.noreply.github.com>
2023-04-14 21:20:48 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -340,3 +340,297 @@ public class Main {
                                     if(orders.size() > 0){
                                         System.out.print("Data Order :");
                                         for(Order order : orders){
+                                            System.out.println("\nID Restaurant : " + order.getIdRestaurant());
+                                            System.out.println("Jarak Lokasi Antar : " + order.getJarak() + " km");
+                                            if(order.getMenuMakanan().size() > 0){
+                                                System.out.println("Menu Makanan :");
+                                                for(int i=0; i < order.getMenuMakanan().size(); i++){
+                                                    System.out.println((i+1) + ". " + order.getMenuMakanan().get(i).getNama() + " ( " + order.getMenuMakanan().get(i).getHarga() + " ) x " + order.getQtyMenuMakanan().get(i));
+                                                }
+                                            }
+
+                                            if(order.getMenuMinuman().size() > 0){
+                                                System.out.println("Menu Minuman :");
+                                                for(int i=0; i < order.getMenuMinuman().size(); i++){
+                                                    System.out.println((i+1) + ". " + order.getMenuMinuman().get(i).getNama() + " ( " + order.getMenuMinuman().get(i).getHarga() + " ) x " + order.getQtyMenuMinuman().get(i));
+                                                }
+                                            }
+
+                                            System.out.println("Total Order : " + order.getTotal());
+                                        }
+                                    }else{
+                                        System.out.println("Data Order Masih Kosong!");
+                                    }
+
+                                    break;
+                                }
+
+                                case 4:{
+                                    System.out.println("Kembali ke login ...");
+
+                                    break;
+                                }
+
+                                default:{
+                                    System.out.println("Pilihan Menu Admin Tidak Valid!");
+
+                                    break;
+                                }
+                            }
+
+                            if(pil != 4){
+                                System.out.print("\n");
+                            }
+                        }while(pil != 4);
+                    }else{
+                        System.out.println("Username atau Password Salah!");
+                    }
+
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("Keluar dari program ...");
+                    
+                    break;
+                }
+
+                default:{
+                    System.out.println("Pilihan Menu Utama Tidak Valid!");
+
+                    break;
+                }
+            }
+
+            if(pil != 2){
+                System.out.println();
+            }import java.util.ArrayList;
+import java.util.Scanner;
+
+            public class Main {
+                public static void main(String[] args) {
+                    Scanner input = new Scanner(System.in);
+                    String username, password;
+                    int pil, id, index, kuantitas, countRestaurant = 0;
+                    ArrayList<Restaurant> restaurants = new ArrayList<>();
+                    ArrayList<Order> orders = new ArrayList<>();
+                    char loop;
+
+                    do{
+                        System.out.println("===Menu Utama===");
+                        System.out.println("1. Login");
+                        System.out.println("2. Keluar");
+                        System.out.print(">> ");
+                        pil = Integer.parseInt(input.nextLine());
+
+                        System.out.println();
+                        switch(pil){
+                            case 1:{
+                                System.out.print("Username : ");
+                                username = input.nextLine();
+
+                                System.out.print("Password : ");
+                                password = input.nextLine();
+
+                                if(username.equals("elma") && password.equals("ruis")){
+                                    do{
+                                        System.out.println("\n===Menu Admin===");
+                                        System.out.println("1. Lihat Restaurant");
+                                        System.out.println("2. Tambah Restaurant");
+                                        System.out.println("3. Hapus Restaurant");
+                                        System.out.println("4. Kembali ke login");
+                                        System.out.print(">> ");
+                                        pil = Integer.parseInt(input.nextLine());
+
+                                        System.out.print("\n");
+                                        switch(pil){
+                                            case 1:{
+                                                if(restaurants.size() > 0){
+                                                    System.out.print("Data Restaurant :");
+                                                    for(Restaurant restaurant : restaurants){
+                                                        System.out.println("\nID      : " + restaurant.getId());
+                                                        System.out.println("Nama    : " + restaurant.getNama());
+                                                        System.out.println("Alamat  : " + restaurant.getAlamat());
+                                                        if(restaurant.getMakanan().size() > 0){
+                                                            System.out.println("Menu Makanan :");
+                                                            for(int i=0; i < restaurant.getMakanan().size(); i++){
+                                                                System.out.println((i+1) + ". " + restaurant.getMakanan().get(i).getNama() + " ( " + restaurant.getMakanan().get(i).getHarga() + " )");
+                                                            }
+                                                        }else{
+                                                            System.out.println("[!] Tidak Ada Menu Makanan");
+                                                        }
+
+                                                        if(restaurant.getMinuman().size() > 0){
+                                                            System.out.println("Menu Minuman :");
+                                                            for(int i=0; i < restaurant.getMinuman().size(); i++){
+                                                                System.out.println((i+1) + ". " + restaurant.getMinuman().get(i).getNama() + " ( " + restaurant.getMinuman().get(i).getHarga() + " )");
+                                                            }
+                                                        }else{
+                                                            System.out.println("[!] Tidak Ada Menu Minuman");
+                                                        }
+                                                    }
+                                                }else{
+                                                    System.out.println("Data Restaurant Masih Kosong!");
+                                                }
+
+                                                break;
+                                            }
+
+                                            case 2:{
+                                                countRestaurant += 1;
+
+                                                Restaurant restoran = new Restaurant();
+
+                                                System.out.print("Masukkan Nama Restaurant : ");
+                                                restoran.setNama(input.nextLine());
+
+                                                System.out.print("\nMasukkan Alamat Restaurant : ");
+                                                restoran.setAlamat(input.nextLine());
+
+                                                while(true) {
+                                                    System.out.print("\nApakah Ingin Menambahkan Menu Makanan [Y/N] : ");
+                                                    loop = input.nextLine().toUpperCase().charAt(0);
+
+                                                    if(loop == 'Y'){
+                                                        Menu makanan = new Menu();
+
+                                                        System.out.print("\nMasukkan Nama Makanan : ");
+                                                        makanan.setNama(input.nextLine());
+
+                                                        System.out.print("\nMasukkan Harga Makanan : ");
+                                                        makanan.setHarga(Long.valueOf(input.nextLine()));
+
+                                                        restoran.addMakanan(makanan);
+                                                    }else if(loop == 'N'){
+                                                        break;
+                                                    }
+                                                }
+
+                                                while(true){
+                                                    System.out.print("\nApakah Ingin Menambahkan Menu Minuman [Y/N] : ");
+                                                    loop = input.nextLine().toUpperCase().charAt(0);
+
+                                                    if(loop == 'Y'){
+                                                        Menu minuman = new Menu();
+
+                                                        System.out.print("\nMasukkan Nama Minuman : ");
+                                                        minuman.setNama(input.nextLine());
+
+                                                        System.out.print("\nMasukkan Harga Minuman : ");
+                                                        minuman.setHarga(Long.valueOf(input.nextLine()));
+
+                                                        restoran.addMinuman(minuman);
+                                                    }else if(loop == 'N'){
+                                                        break;
+                                                    }
+                                                }
+
+                                                restoran.setId(countRestaurant);
+
+                                                restaurants.add(restoran);
+                                                System.out.println("\nData Restaurant Berhasil ditambahkan!");
+
+                                                break;
+                                            }
+
+                                            case 3:{
+                                                System.out.print("Masukkan Id Restaurant yang ingin dihapus : ");
+                                                id = Integer.parseInt(input.nextLine());
+
+                                                index = -1;
+                                                for(int i=0; i<restaurants.size(); i++){
+                                                    if(restaurants.get(i).getId() == id){
+                                                        index = i;
+                                                        break;
+                                                    }
+                                                }
+
+                                                if(index != -1){
+                                                    restaurants.remove(index);
+                                                    System.out.println("Berhasil Menghapus Data Restraunt dengan ID " + id);
+                                                }else{
+                                                    System.out.println("Gagal Menghapus Data Restaurant dengan ID " + id + ". ID Tidak ditemukan!");
+                                                }
+
+                                                break;
+                                            }
+
+                                            case 4:{
+                                                System.out.println("Kembali ke login ...");
+
+                                                break;
+                                            }
+
+                                            default:{
+                                                System.out.println("Pilihan Menu Admin Tidak Valid!");
+
+                                                break;
+                                            }
+                                        }
+
+                                        if(pil != 4){
+                                            System.out.print("\n");
+                                        }
+                                    }while(pil != 4);
+                                }else if(username.equals("user") && password.equals("user")){
+                                    do{
+                                        System.out.println("\n===Menu User===");
+                                        System.out.println("1. Lihat Restaurant");
+                                        System.out.println("2. Buat Pesanan");
+                                        System.out.println("3. Lihat Pesanan");
+                                        System.out.println("4. Kembali ke login");
+                                        System.out.print(">> ");
+                                        pil = Integer.parseInt(input.nextLine());
+
+                                        System.out.print("\n");
+                                        switch(pil){
+                                            case 1:{
+                                                if(restaurants.size() > 0){
+                                                    System.out.print("Data Restaurant :");
+                                                    for(Restaurant restaurant : restaurants){
+                                                        System.out.println("\nID      : " + restaurant.getId());
+                                                        System.out.println("Nama    : " + restaurant.getNama());
+                                                        System.out.println("Alamat  : " + restaurant.getAlamat());
+                                                        if(restaurant.getMakanan().size() > 0){
+                                                            System.out.println("Menu Makanan :");
+                                                            for(int i=0; i < restaurant.getMakanan().size(); i++){
+                                                                System.out.println((i+1) + ". " + restaurant.getMakanan().get(i).getNama() + " ( " + restaurant.getMakanan().get(i).getHarga() + " )");
+                                                            }
+                                                        }else{
+                                                            System.out.println("[!] Tidak Ada Menu Makanan");
+                                                        }
+
+                                                        if(restaurant.getMakanan().size() > 0){
+                                                            System.out.println("Menu Minuman :");
+                                                            for(int i=0; i < restaurant.getMinuman().size(); i++){
+                                                                System.out.println((i+1) + ". " + restaurant.getMinuman().get(i).getNama() + " ( " + restaurant.getMinuman().get(i).getHarga() + " )");
+                                                            }
+                                                        }else{
+                                                            System.out.println("[!] Tidak Ada Menu Minuman");
+                                                        }
+                                                    }
+                                                }else{
+                                                    System.out.println("Data Restaurant Masih Kosong!");
+                                                }
+
+                                                break;
+                                            }
+
+                                            case 2:{
+                                                if(restaurants.size() > 0){
+                                                    System.out.print("Masukkan ID Restaurant : ");
+                                                    id = Integer.parseInt(input.nextLine());
+
+                                                    index = -1;
+                                                    for(int i=0; i<restaurants.size(); i++){
+                                                        if(restaurants.get(i).getId() == id){
+                                                            index = i;
+                                                            break;
+                                                        }
+                                                    }
+
+                                                    if(index != -1){
+                                                        ArrayList<Menu> menu;
+                                                        long total = 0;
+                                                        Order order = new Order();
+                                                        order.setIdRestaurant(restaurants.get(index).getId());
